4db9da1dfb9020a580ece1f9ffda2ba4d3fa552e
Masahiro Kanai <cani.m.61st@gmail.com>
2016-03-17 01:33:07 +0900

    append rel=noopener for target=_blank links

--- _plugins/githublinktag.rb
+++ _plugins/githublinktag.rb
@@ -8,7 +8,7 @@ module Jekyll
       @text = text.strip
     end
     def render(context)
-      "<a class=\"btn btn-primary btn-xs fui-github\" href=\"https://github.com/mkanai/#{@text}\" target=\"_blank\"> GitHub </a> <a href=\"https://travis-ci.org/mkanai/#{@text}\" target=\"_blank\"><img src=\"https://travis-ci.org/mkanai/#{@text}.svg?branch=master\" alt=\"Build Status\"></a>"
+      "<a class=\"btn btn-primary btn-xs fui-github\" href=\"https://github.com/mkanai/#{@text}\" target=\"_blank\" rel=\"noopener\"> GitHub </a> <a href=\"https://travis-ci.org/mkanai/#{@text}\" target=\"_blank\" rel=\"noopener\"><img src=\"https://travis-ci.org/mkanai/#{@text}.svg?branch=master\" alt=\"Build Status\"></a>"
     end
   end
 end
--- _plugins/jekyll-scholar.rb
+++ _plugins/jekyll-scholar.rb
@@ -17,9 +17,9 @@ module Jekyll
         # highlight authorship
         reference = reference.gsub(/Kanai, M|金井 仁弘/u){|c| "<b>#{$&}</b>"}
         # link doi
-        reference = reference.gsub(/\bdoi:(10[.][0-9]{4,}(?:[.][0-9]+)*\/(?:(?![\"&\'<>])\S)+)\b/) { |c| "<a href=\"http://dx.doi.org/#{$1}\" target=\"_blank\">#{$&}</a>" }
+        reference = reference.gsub(/\bdoi:(10[.][0-9]{4,}(?:[.][0-9]+)*\/(?:(?![\"&\'<>])\S)+)\b/) { |c| "<a href=\"http://dx.doi.org/#{$1}\" target=\"_blank\" rel=\"noopener\">#{$&}</a>" }
         # link urls
-        reference = reference.gsub(/\[([^\]]*)\]\(((?:(?:https?|ftp):\/)?\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)\)/) { |c| "<a class=\"btn btn-primary btn-xs\" href=\"#{$2}\" target=\"_blank\">#{$1}</a>" }
+        reference = reference.gsub(/\[([^\]]*)\]\(((?:(?:https?|ftp):\/)?\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)\)/) { |c| "<a class=\"btn btn-primary btn-xs\" href=\"#{$2}\" target=\"_blank\" rel=\"noopener\">#{$1}</a>" }
 
         content_tag reference_tagname, reference,
           :id => [prefix, entry.key].compact.join('-')
