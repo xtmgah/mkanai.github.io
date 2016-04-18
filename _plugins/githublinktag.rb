47971c5c3ee3e459843897a949f2991baf864c8c
Masahiro Kanai <cani.m.61st@gmail.com>
2016-04-19 00:11:32 +0900

    now, use extra '!' to add travis budge

--- _plugins/githublinktag.rb
+++ _plugins/githublinktag.rb
@@ -5,10 +5,16 @@ module Jekyll
   class GitHubLinkTag < Liquid::Tag
     def initialize(tag_name, text, tokens)
       super
-      @text = text.strip
+      @repo = text.strip
+      @travis = @repo.end_with?("!")
+      @repo = @repo.chop if @travis
     end
     def render(context)
-      "<a class=\"btn btn-primary btn-xs fui-github\" href=\"https://github.com/mkanai/#{@text}\" target=\"_blank\" rel=\"noopener\"> GitHub </a> <a href=\"https://travis-ci.org/mkanai/#{@text}\" target=\"_blank\" rel=\"noopener\"><img src=\"https://travis-ci.org/mkanai/#{@text}.svg?branch=master\" alt=\"Build Status\"></a>"
+      content = "<a class=\"btn btn-primary btn-xs fui-github\" href=\"https://github.com/mkanai/#{@repo}\" target=\"_blank\" rel=\"noopener\"> GitHub </a>"
+      if @travis
+        content += " <a href=\"https://travis-ci.org/mkanai/#{@repo}\" target=\"_blank\" rel=\"noopener\"><img src=\"https://travis-ci.org/mkanai/#{@repo}.svg?branch=master\" alt=\"Build Status\"></a>"
+      end
+      content
     end
   end
 end
